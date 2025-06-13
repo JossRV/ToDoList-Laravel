eff40a0740760642ce91b8a07358c520b72fe083
JossRV <jossrv74@gmail.com>
2025-06-13 01:15:44 -0600

    feat(routing): define new app and manager routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,12 +3,24 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
+        then: function () {
+            Route::middleware('web')
+                ->prefix('manager')
+                ->name('manager.')
+                ->group(base_path('routes/manager.php'));
+
+            Route::middleware('web')
+                ->prefix('app')
+                ->name('app.')
+                ->group(base_path('routes/app.php'));
+        }
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
